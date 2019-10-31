65ff90160d528ed77fae72211c721e812e188106
Andrey Dovbnya <andrey.dovbnya@namecheap.com>
2019-10-31 18:46:27 +0200

    add default plugins

--- Dockerfile
+++ Dockerfile
@@ -17,5 +17,5 @@ RUN curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - \
 
 USER jenkins
 
-# COPY plugins.txt /usr/share/jenkins/ref/plugins.txt
-# RUN /usr/local/bin/install-plugins.sh < /usr/share/jenkins/ref/plugins.txt
+COPY default_plugins.txt /usr/share/jenkins/ref/plugins.txt
+RUN /usr/local/bin/install-plugins.sh < /usr/share/jenkins/ref/plugins.txt
--- run.ps1
+++ run.ps1
@@ -1,2 +1,2 @@
 #docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v C:\jenkins_home:/var/jenkins_home --rm --entrypoint bash ffapch/jenkins:2.0
-docker run -it -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v C:\jenkins_home:/var/jenkins_home --rm ffapch/jenkins:2.0
+docker run -it -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v C:\jenkins_home:/var/jenkins_home ffapch/jenkins:2.0
